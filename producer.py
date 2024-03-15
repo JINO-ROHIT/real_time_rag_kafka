1cb6f86f1ac75b8aa276928b076cddabfe19afcb
JINO-ROHIT <find.jinorohit@gmail.com>
2024-03-15 21:48:14 +0530

    producer pipe

--- producer.py
+++ producer.py
@@ -4,8 +4,6 @@ import random
 
 producer = KafkaProducer(bootstrap_servers = 'localhost:9092')
 
-WEATHER_STATES = ['Sunny', 'Rainy', 'Windy', 'Cloudy', 'Snowy']
-
 while True:
     query = input("ask your question: ")
     producer.send('myfirsttopic', value = query.encode('utf-8'))
